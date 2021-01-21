08a596d8abb8ea40ac1b75ea881ec0550363f572
vgavhane <vaibhav.gavhane9@gmail.com>
2021-01-22 01:10:21 +0530

    initial commits for angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'csv-uploader';
+   
+  csvRecords: any[] = [];
+  header = true;
+  filterTerm: string;
+  constructor(private ngxCsvParser: NgxCsvParser) {
+  }
+ 
+  @ViewChild('fileImportInput', { static: false }) fileImportInput: any;
+ 
+  // Your applications input change listener for the CSV File
+  fileChangeListener($event: any): void {
+ 
+    // Select the files from the event
+    const files = $event.srcElement.files;
+ 
+    // Parse the file you want to select for the operation along with the configuration
+    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
+      .pipe().subscribe((result: Array<any>) => {
+ 
+        console.log('Result', result);
+        this.csvRecords = result;
+      }, (error: NgxCSVParserError) => {
+        console.log('Error', error);
+      });
+ 
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
+import { NgxCsvParserModule } from 'ngx-csv-parser';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+
 
 @NgModule({
-  declarations: [
+  
+declarations: [
     AppComponent
   ],
-  imports: [
+  
+imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  NgxCsvParserModule ,  AppRoutingModule
+ , FormsModule , Ng2SearchPipeModule ],
+  
+providers: [],
+ 
+ bootstrap: [AppComponent]
 })
+
 export class AppModule { }
